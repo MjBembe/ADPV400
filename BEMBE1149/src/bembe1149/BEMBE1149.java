5dffd764824987e1ac9982745ba84b13fdc8ac75
MJ Bembe <mncedisie@gmail.com>
2020-04-08 14:15:31 +0200

    Merge origin/master
    Conflicts:
    	README.md

---
+++ BEMBE1149/src/bembe1149/BEMBE1149.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bembe1149;
+
+/**
+ *
+ * @author Mncedisi.Bembe
+ */
+public class BEMBE1149 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int x = 4;
+        int y= 71;
+        int z= x + y;
+        System.out.println("The sum of x = "+x+" and y = "+y+" is equal to "+z);
+        // TODO code application logic here
+    }
+    
+}
